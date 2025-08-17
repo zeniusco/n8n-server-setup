46e488f760e1a51f4a67ffa062d791e75774a68c
zeniusco <admin@zenius.co>
2025-08-17 14:11:15 +0530

    Delete n8n-data/postgres-backup.sh

--- n8n-data/postgres-backup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-PGPASSWORD=yourpassword pg_dump -U n8nuser -h 127.0.0.1 n8ndb > /home/runcloud/webapps/n8n/n8n-data/postgres/pg_backup_$(date +\%F).sql
-if [ $? -ne 0 ]; then
-  echo "n8n PostgreSQL backup FAILED at $(date) on $(hostname)" | mail -s "ERROR: n8n PostgreSQL Backup Failed" youremail@yourdomain.com
-fi
